e58d1267e5bb817faebd0da83350a339eb833c66
arduriki <jordi.ardura.professional@gmail.com>
2023-12-14 13:00:44 +0100

    QR code scanner implemented

--- app/src/main/java/com/teknos/m8uf2/jardura/QrCodeScannerActivity.java
+++ app/src/main/java/com/teknos/m8uf2/jardura/QrCodeScannerActivity.java
@@ -1,14 +1,197 @@
 package com.teknos.m8uf2.jardura;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentManager;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.graphics.Matrix;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Size;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mlkit.vision.barcode.BarcodeScanner;
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
+import com.google.mlkit.vision.barcode.BarcodeScanning;
+import com.google.mlkit.vision.barcode.common.Barcode;
+import com.google.mlkit.vision.common.InputImage;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class QrCodeScannerActivity extends AppCompatActivity {
+
+    private ListenableFuture cameraProviderFuture;
+    private ExecutorService cameraExecutor;
+    private PreviewView previewView;
+    private MyImageAnalyzer analyzer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr_code_scanner);
+
+        previewView = findViewById(R.id.previewView);
+        this.getWindow().setFlags(1024, 1024);
+
+        // Background job
+        cameraExecutor = Executors.newSingleThreadExecutor();
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
+        analyzer = new MyImageAnalyzer(getSupportFragmentManager());
+
+        // Camera provider future
+        cameraProviderFuture.addListener(new Runnable() {
+            @Override
+            public void run() {
+                // In background job
+                try {
+                    if (ActivityCompat.checkSelfPermission(QrCodeScannerActivity.this, Manifest.permission.CAMERA) != (PackageManager.PERMISSION_GRANTED)) {
+                        // Ask for permission to use the camera
+                        ActivityCompat.requestPermissions(QrCodeScannerActivity.this, new String[]{Manifest.permission.CAMERA}, 101);
+                    } else {
+                        ProcessCameraProvider processCameraProvider = (ProcessCameraProvider) cameraProviderFuture.get();
+                        bindPreview(processCameraProvider);
+                    }
+                } catch (ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    // Request permissions from the user
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 101 && grantResults.length > 0) {
+            ProcessCameraProvider processCameraProvider = null;
+            try {
+                processCameraProvider = (ProcessCameraProvider) cameraProviderFuture.get();
+            } catch (ExecutionException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            bindPreview(processCameraProvider);
+        }
+    }
+
+    private void bindPreview(ProcessCameraProvider processCameraProvider) {
+        Preview preview = new Preview.Builder().build();
+        CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(
+                CameraSelector.LENS_FACING_BACK).build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        ImageCapture imageCapture = new ImageCapture.Builder().build();
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                .setTargetResolution(new Size(1280, 720))
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build();
+
+        imageAnalysis.setAnalyzer(cameraExecutor, analyzer);
+        processCameraProvider.unbindAll();
+        processCameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture, imageAnalysis);
+
+    }
+
+
+    // ImageAnalyzer class
+    public class MyImageAnalyzer implements ImageAnalysis.Analyzer {
+
+        private FragmentManager fragmentManager;
+        private BottomDialog bottomDialog;
+
+        public MyImageAnalyzer(FragmentManager supportFragmentManager) {
+            this.fragmentManager = supportFragmentManager;
+            bottomDialog = new BottomDialog();
+        }
+
+        @Override
+        public void analyze(@NonNull ImageProxy image) {
+            ScanBarCode(image);
+        }
+
+        private void ScanBarCode(ImageProxy image) {
+            @SuppressLint("UnsafeOptInUsageError") Image image1 = image.getImage();
+            assert image1 != null;
+            InputImage inputImage = InputImage.fromMediaImage(image1, image.getImageInfo().getRotationDegrees());
+            BarcodeScannerOptions scannerOptions =
+                    new BarcodeScannerOptions.Builder()
+                            .setBarcodeFormats(
+                                    Barcode.FORMAT_QR_CODE,
+                                    Barcode.FORMAT_AZTEC
+                            ).build();
+
+            BarcodeScanner scanner = BarcodeScanning.getClient(scannerOptions);
+            Task<List<Barcode>> result = scanner.process(inputImage)
+                    .addOnSuccessListener(new OnSuccessListener<List<Barcode>>() {
+                        @Override
+                        public void onSuccess(List<Barcode> barcodes) {
+                            readerBarcodeData(barcodes);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            // On case of failing to read the QR code
+                            Toast.makeText(QrCodeScannerActivity.this, "Fail to read the code", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnCompleteListener(new OnCompleteListener<List<Barcode>>() {
+                        @Override
+                        public void onComplete(@NonNull Task<List<Barcode>> task) {
+                            image.close();
+                        }
+                    });
+        }
+
+        private void readerBarcodeData(List<Barcode> barcodes) {
+            for (Barcode barcode : barcodes) {
+                Rect bounds = barcode.getBoundingBox();
+                Point[] corners = barcode.getCornerPoints();
+
+                String rawValue = barcode.getRawValue();
+
+                int valueType = barcode.getValueType();
+
+                // Options to scan for
+                switch (valueType) {
+                    case Barcode.TYPE_PRODUCT:
+                        // To retrieve product's barcode number
+                        String productCode = barcode.getDisplayValue();
+                        break;
+                    case Barcode.TYPE_URL:
+                        // To retrieve the product's medical information
+                        if (bottomDialog.isAdded()) {
+                            bottomDialog.show(fragmentManager, "");
+                        }
+                        bottomDialog.setFetchURL(barcode.getUrl().getUrl());
+                        String title = barcode.getUrl().getTitle();
+                        String url = barcode.getUrl().getUrl();
+                        break;
+                }
+            }
+        }
     }
 }
