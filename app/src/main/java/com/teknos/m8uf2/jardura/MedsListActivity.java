d68cd496708c841bc9558c78de30b8299ac6c639
arduriki <jordi.ardura.professional@gmail.com>
2023-12-14 09:43:48 +0100

    Splash screen and add button added

--- app/src/main/java/com/teknos/m8uf2/jardura/MedsListActivity.java
+++ app/src/main/java/com/teknos/m8uf2/jardura/MedsListActivity.java
@@ -2,7 +2,11 @@ package com.teknos.m8uf2.jardura;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MedsListActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MedsListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meds_list);
+
+        FloatingActionButton addButton = findViewById(R.id.addBtn);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Launch QR code scanner
+                Intent i = new Intent(getApplicationContext(), QrCodeScannerActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/teknos/m8uf2/jardura/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.teknos.m8uf2.jardura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Loading process and how long will it last.
+        final long DELAY = 3000; // 3 secs
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, DELAY);
+    }
+}
